db832b89b58a436d14c1471d33d2c448fb715669
Faustino Martinez <faustmtz@protonmail.com>
2018-09-13 20:29:12 -0500

    search for people route added

--- routes/search.js
+++ routes/search.js
@@ -4,8 +4,23 @@ const router = express.Router();
 
 const API = '?api_key=f7490bb2d22f0d184c552b8a15b5b733';
 
+/* GET request to search for movies */
+
 router.get('/movie/:name/:page', (req, res) => {
 	fetch(`https://api.themoviedb.org/3/search/movie${API}&language=en-US&query=${req.params.name}&page=${req.params.page}&include_adult=false`)
+		.then((data) => {
+			return (data.json());
+		})
+		.then((json) => {
+			console.log(json);
+			res.send(json);
+	});
+});
+
+/* GET request to search for people */
+
+router.get('/person/:name/:page', (req, res) => {
+	fetch(`https://api.themoviedb.org/3/search/person${API}&language=en-US&query=${req.params.name}&page=${req.params.page}&include_adult=false`)
 		.then((data) => {
 			return (data.json());
 		})
