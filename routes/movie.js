9471542835f6650fab684ffeee04bfdf35b5527a
Faustino Martinez <faustmtz@protonmail.com>
2018-09-16 17:02:53 -0500

    upcoming and most popular movie routes added

--- routes/movie.js
+++ routes/movie.js
@@ -35,6 +35,32 @@ router.get('/now_playing/:page', (req, res) => {
 	});
 });
 
+/* GET Route to get upcoming movies */
+
+router.get('/upcoming/:page', (req, res) => {
+	fetch(`https://api.themoviedb.org/3/movie/upcoming${API}&language=en-US&page=${req.params.page}`)
+		.then((data) => {
+			return (data.json());
+		})
+		.then((json) => {
+			console.log(json);
+			res.send(json);
+	});
+});
+
+/* GET Route to get the most popular movies */
+
+router.get('/popular/:page', (req, res) => {
+	fetch(`https://api.themoviedb.org/3/movie/popular${API}&language=en-US&page=${req.params.page}`)
+		.then((data) => {
+			return (data.json());
+		})
+		.then((json) => {
+			console.log(json);
+			res.send(json);
+	});
+});
+
 /* GET Route to get movie reviews */
 
 router.get('/:movie_id/reviews', (req, res) => {
