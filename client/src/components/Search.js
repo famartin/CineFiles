47c538c80a1c9dbb18fc14b5a28a28e5c3613105
Faustino Martinez <faustmtz@protonmail.com>
2018-09-21 17:02:07 -0500

    header bg image added

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -10,20 +10,16 @@ class Search extends Component {
 					<input type="hidden" name="_csrf" value="XeiXGVjn-Xs_3aSk_7iMGzh84PgeyMRWRwr4" />
 					<div className="field has-addons">
 						<div id="search-field-control" className="control is-large is-expanded">
-							<input className="input is-large has-text-centered" type="text" id="search-field" name="q" autocomplete="off" />
+							<input className="input is-large has-text-centered" type="text" id="search-field" name="q" autoComplete="off" placeholder="type a movie, tv show or person" />
 						</div>
-						<div class="control">
-							<button type="submit" class="button is-large" id="search-button">
-								<span class="icon is-medium is-right">
-									<i class="fa fa-search"></i>
+						<div className="control">
+							<button type="submit" className="button is-large" id="search-button">
+								<span className="icon is-medium is-right">
+									<i className="fa fa-search"></i>
 								</span>
 							</button>
 						</div>
 					</div>
-					<p class="help has-text-centered">
-						For example: <strong>harry styles - sign of the times</strong>
-						<span class="is-hidden-mobile">(which is 120 BPM, by the way)</span>
-					</p>
 				</form>
 			</div>
 		</section>
