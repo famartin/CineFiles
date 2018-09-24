6349575622a243ba96faf4149e4e617c6a08dee2
Faustino Martinez <faustmtz@protonmail.com>
2018-09-24 00:23:49 -0500

    main component being filled in

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
 
 class Main extends Component {
-  render() {
-    return (
-		<p>Main</p>
-    );
-  }
+	constructor() {
+		super();
+	}
+
+	array = [
+		'movie 1',
+		'movie 2',
+		'movie 3',
+		'movie 4',
+		'movie 5',
+		'movie 6',
+		'movie 7',
+	];
+
+	render() {
+    	return (
+			<section className="section main">
+				<div className="container">
+					<div class="tile is-ancestor is-vertical">
+						<div class="tile is-vertical is-parent">
+							{
+								this.array.map((string) => {
+        							return <p>{string}</p>;
+								})
+							}
+						</div>
+					</div>
+				</div>
+			</section>
+		);
+	}
 }
 
 export default Main;
