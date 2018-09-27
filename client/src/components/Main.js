0cec5bd9b4a6ab7ec47e80d76656fb8a623790be
Faustino Martinez <faustmtz@protonmail.com>
2018-09-26 23:45:55 -0500

    pagination started

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -3,6 +3,40 @@ import Card from './Card';
 import './Main.css';
 
 class Main extends Component {
+	constructor() {
+		super();
+		this.state = {
+			currentPage: 1
+		};
+		this.handleClick = this.handleClick.bind(this);
+	}
+
+	handleClick(event) {
+        this.setState({
+          currentPage: Number(event.target.id)
+		});
+		console.log(event.target.id);
+		console.log(this.state.currentPage);
+    }
+
+	createPagination = () => {
+		let pages = [];
+	
+		for (let i = 1; i < this.props.passedData.total_pages; i++) {
+			pages.push(
+				<li className="page-item" key={i}>
+					<a className="page-link" href="#"
+						id={i}
+						onClick={this.handleClick}
+					>
+						{i}
+					</a>
+				</li>
+			);
+		}
+		return pages;
+	}
+
 	render() {
     	return (
 			<section className="section main">
@@ -23,8 +57,20 @@ class Main extends Component {
 					</ul>
 				</div>
 				<div className="container">
+					<nav aria-label="Page navigation example">
+						<ul className="pagination">
+							<li className="page-item"><a className="page-link" href="#">Previous</a></li>
+							<li className="page-item"><a className="page-link" href="#">1</a></li>
+							{this.props.passedData && this.createPagination()}
+							<li className="page-item"><a className="page-link" href="#">Next</a></li>
+						</ul>
+					</nav>
+				</div>
+				<br />
+				<div className="container">
 					<div className="tile is-ancestor is-vertical">
 						<div className="tile is-vertical is-parent">
+							<h5>Pages: {this.props.passedData && this.props.passedData.total_pages}</h5><br />
 							{
 								this.props.passedData && this.props.passedData.results.map((item) => {
 									return (<Card
