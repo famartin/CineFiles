3907c0dddb05d7331f1563aa603b510009767d7a
Faustino Martinez <faustmtz@protonmail.com>
2018-09-28 17:08:40 -0500

    10 pages

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -25,7 +25,7 @@ class Main extends Component {
 		var numOfPages = this.props.passedData.total_pages;
 
 		if (this.props.passedData.total_pages >= 10) {
-			numOfPages = 10;
+			numOfPages = 11;
 		}
 	
 		for (let i = 1; i < numOfPages; i++) {
@@ -66,7 +66,6 @@ class Main extends Component {
 					<nav aria-label="Page navigation example">
 						<ul className="pagination">
 							<li className="page-item"><a className="page-link" href="#">Previous</a></li>
-							<li className="page-item"><a className="page-link" href="#">1</a></li>
 							{this.props.passedData && this.createPagination()}
 							<li className="page-item"><a className="page-link" href="#">Next</a></li>
 						</ul>
