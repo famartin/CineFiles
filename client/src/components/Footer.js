833e6fe28a742161fd46bcb08a3e7ea68729b617
Faustino Martinez <faustmtz@protonmail.com>
2018-10-10 22:25:17 -0500

    styling added to pagination

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -8,12 +8,12 @@ class Footer extends Component {
 				<div className="container-fluid">
 					<div className="row">
 						<div className="col-md-8">
-							<img className="tmdb-logo" src="/assets/tmdb-logo.png" /><br />
+							<img className="tmdb-logo" src="/assets/tmdb-logo.png" alt="The Movie Database Logo" /><br />
 							<small>This product uses the TMDb API but is not endorsed or certified by TMDb.</small><br />
 						</div>
 						<div className="col-md-4">
 							<h3>Thanks for stopping by...</h3>
-							<p>Made with ❤️ by <a target="blank" href="https://famartin.github.io">famartin</a><br /></p>
+							<p>Made with <span role="img" aria-label="Heart Emoji">❤️</span> by <a target="blank" href="https://famartin.github.io">famartin</a><br /></p>
 							<small>&#169; famartin 2018</small>
 						</div>
 					</div>
