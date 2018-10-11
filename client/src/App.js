8a6478f7938e38222ad9fafe82e8e83995bfdcc5
Faustino Martinez <faustmtz@protonmail.com>
2018-10-11 14:41:27 -0500

    Error component added

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Home from './components/Home';
 import InfoPage from './components/InfoPage';
+import Error from './components/Error';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div>
+        <Switch>
         <Route path="/id" component={InfoPage}/>
         <Route path="/" component={Home} exact />
-        </div>
+        <Route component={Error} />
+        </Switch>
       </BrowserRouter>
     );
   }
