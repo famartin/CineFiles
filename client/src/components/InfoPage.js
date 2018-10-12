e15376de4ee76bf7dd99f9c2f070596962da56d0
Faustino Martinez <faustmtz@protonmail.com>
2018-10-11 23:52:06 -0500

    Header component added to InfoPage

--- client/src/components/InfoPage.js
+++ client/src/components/InfoPage.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 
+import Header from './Header';
+
 class InfoPage extends Component {
 	render() {
 		return (
-			<p>Hi, this is the infoPage</p>
+			<div className="infoPage">
+				<Header />
+				<div className="container">
+					<div className="row">
+						<div className="col-md-12">
+							<h3 className="text-center">
+								Movie/Series Name
+							</h3>
+						</div>
+					</div>
+				</div>
+			</div>
 		);
 	}
 }
